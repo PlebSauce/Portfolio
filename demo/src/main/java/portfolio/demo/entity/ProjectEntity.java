db425b8914846fac13b21c877b8fe2b24aa4b3c6
Jordan <119638801+PlebSauce@users.noreply.github.com>
2023-12-20 23:53:21 -0800

    Add getters and setters. Fixed thymeleaf bugs.

--- demo/src/main/java/portfolio/demo/entity/ProjectEntity.java
+++ demo/src/main/java/portfolio/demo/entity/ProjectEntity.java
@@ -21,4 +21,18 @@ public class ProjectEntity {
 
     private LocalDate endDate;
 
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
 }
