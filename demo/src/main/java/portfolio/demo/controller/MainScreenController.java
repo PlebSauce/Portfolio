01f9f0474b8b473d6dbb5991de8d8349613a3c95
Jordan <119638801+PlebSauce@users.noreply.github.com>
2024-02-06 10:54:58 -0800

    Fix background position and styling. Anchor footer to bottom and nav bar to fix at top.

--- demo/src/main/java/portfolio/demo/controller/MainScreenController.java
+++ demo/src/main/java/portfolio/demo/controller/MainScreenController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import portfolio.demo.entity.ProjectEntity;
 import portfolio.demo.entity.UserEntity;
 import portfolio.demo.service.ProjectService;
