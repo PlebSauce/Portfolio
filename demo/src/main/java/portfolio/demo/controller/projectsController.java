ac4f7718faf225f5bf5198f0022f9d9a33202b25
Jordan <119638801+PlebSauce@users.noreply.github.com>
2024-02-06 18:27:20 -0800

    Add getters and setters and start controller logic.

--- demo/src/main/java/portfolio/demo/controller/projectsController.java
+++ demo/src/main/java/portfolio/demo/controller/projectsController.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import portfolio.demo.service.ProjectService;
+
 @Controller
 @CrossOrigin
-public class projectsController {
-    
+public class ProjectsController {
+
+    private ProjectService projectService;
+
+    public ProjectsController(ProjectService projectService){
+        this.projectService = projectService;
+    }
+
     @GetMapping("/projects")
     public String listProjects(){
         return "projects";
--- demo/src/main/java/portfolio/demo/entity/ProjectEntity.java
+++ demo/src/main/java/portfolio/demo/entity/ProjectEntity.java
@@ -2,14 +2,15 @@ package portfolio.demo.entity;
 
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import java.time.LocalDate;
 
 @Entity
 public class ProjectEntity {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Nonnull
@@ -17,22 +18,43 @@ public class ProjectEntity {
 
     private String details;
 
-    private LocalDate startDate;
+    private String startDate;
 
-    private LocalDate endDate;
+    private String endDate;
 
     public Long getId() {
         return id;
     }
-    
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
-    }
-    
+    }  
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getDetails() {
+        return details;
+    }
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+    
 }
