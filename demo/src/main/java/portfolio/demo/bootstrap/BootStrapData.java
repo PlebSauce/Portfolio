e1381921473607b44a2279118921c4ddc1ad7739
Jordan <119638801+PlebSauce@users.noreply.github.com>
2024-02-11 16:52:06 -0800

    Implement transition effect upon scrolling for project elements.

--- demo/src/main/java/portfolio/demo/bootstrap/BootStrapData.java
+++ demo/src/main/java/portfolio/demo/bootstrap/BootStrapData.java
@@ -32,6 +32,29 @@ public class BootStrapData implements CommandLineRunner {
             project2.setDetails("This is just some example description to fill in later! But this one is going to have some more than the other...");
             project2.setStartDate("5/25/2023");
             projectRepository.save(project2);
+
+            ProjectEntity project3 = new ProjectEntity();
+            //project2.setId((long)2);
+            project3.setName("A different example");
+            project3.setDetails("This is just some example description to fill in later! I really wanted to make this one long just to see what would happen so i just kept" +
+            " typing and typing over and over again Yet somehow the description needs even more. I wonder how to span more lines? Maybe I need to make a whole seperate text field." + 
+            " Maybe then I'll be able to have enough information...");
+            project3.setStartDate("5/25/2023");
+            projectRepository.save(project3);
+
+            ProjectEntity project4 = new ProjectEntity();
+            //project2.setId((long)2);
+            project4.setName("A different example");
+            project4.setDetails("This is just some example description to fill in later! But this one is going to have some more than the other, no libraries needed for this! Isn't that just crazy?");
+            project4.setStartDate("5/25/2023");
+            projectRepository.save(project4);
+
+            ProjectEntity project5 = new ProjectEntity();
+            //project2.setId((long)2);
+            project5.setName("A different example");
+            project5.setDetails("This is just some example description to fill in later! But this one is going to have some more than the other. I used 57,000 libraries, woah...");
+            project5.setStartDate("5/25/2023");
+            projectRepository.save(project5);
         }
     }
 }
