e3b585460b70c29bb91dc8fd7d98b59b2bc343c3
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-03-12 15:33:44 -0700

    Basic progress bar add. Saving just to revert back to this version while I test this new method.

--- demo/src/main/resources/static/js/app.js
+++ demo/src/main/resources/static/js/app.js
@@ -13,5 +13,18 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.list-projects');
 hiddenElements.forEach((el) => observer.observe(el)); 
 
-const hiddenElements2 = document.querySelectorAll('.about-me-content');
+const hiddenElements2 = document.querySelectorAll('.about-transition');
 hiddenElements2.forEach((el) => observer.observe(el));
+
+let number = document.getElementById('number');
+let counter = 0;
+
+setInterval(() => {
+    if (counter == 65) {
+        clearInterval();
+    }
+    else {
+        counter += 1;
+        number.innerHTML = `${counter}%`;
+    }
+}, 20);
